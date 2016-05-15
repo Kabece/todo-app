a289c79e29c55cc8e5184e799f0aaa8b2bb1e622
durden2 <gandor.michal@gmail.com>
2016-05-15 23:06:56 +0200

    Added new datepicker

--- public/js/controllers/main.js
+++ public/js/controllers/main.js
@@ -4,7 +4,7 @@ angular.module('todoController', [])
     $scope.addTodo = false;
     $scope.listName = "Listy";
 
-    $scope.toggle = function() {
+     $scope.toggle = function() {
       $scope.addTodo = !$scope.addTodo;
     }
 
@@ -55,4 +55,22 @@ angular.module('todoController', [])
                $scope.todoHistory = todos[1];
              });
       }
+
+    $scope.myDate = new Date();
+
+    $scope.minDate = new Date(
+        $scope.myDate.getFullYear(),
+        $scope.myDate.getMonth() - 2,
+        $scope.myDate.getDate());
+
+    $scope.maxDate = new Date(
+        $scope.myDate.getFullYear(),
+        $scope.myDate.getMonth() + 2,
+        $scope.myDate.getDate());
+
+    $scope.onlyWeekendsPredicate = function(date) {
+      var day = date.getDay();
+      return day === 0 || day === 6;
+    }
   });
+
